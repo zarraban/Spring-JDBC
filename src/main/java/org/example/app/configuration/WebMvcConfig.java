d90f0c17fec071b2f216775b9ee557d5e115461c
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-18 23:59:43 +0300

    Add SpringMVC configuration

---
+++ src/main/java/org/example/app/configuration/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package org.example.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.example.app")
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
