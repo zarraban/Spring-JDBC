cf5da72172829eb38176fb62df57937b03e0d0a7
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-18 14:17:15 +0300

    Add CRUD methods to CustomerService class

---
+++ src/main/java/org/example/app/service/customer/CustomerService.java
@@ -0,0 +1,8 @@
+package org.example.app.service.customer;
+
+import org.example.app.dto.CustomerDTOrequest;
+import org.example.app.entity.Customer;
+import org.example.app.service.BaseService;
+
+public interface CustomerService extends BaseService<CustomerDTOrequest, Customer> {
+}
