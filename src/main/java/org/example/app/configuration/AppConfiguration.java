8fea73ae0a97b65d3e7b118584fa8cb1d725e7c2
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-18 14:19:05 +0300

    Add transactions management to config

--- src/main/java/org/example/app/configuration/AppConfiguration.java
+++ src/main/java/org/example/app/configuration/AppConfiguration.java
@@ -7,12 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Objects;
 
 @Configuration
+@EnableTransactionManagement
 @PropertySource({"classpath:app.properties"})
 public class AppConfiguration {
     @Autowired
@@ -30,7 +34,12 @@ public class AppConfiguration {
     }
 
     @Bean
-    public JdbcTemplate getJdbcTemplate(){
-        return new JdbcTemplate(getDataSource());
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
     }
 }
